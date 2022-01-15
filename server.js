2963f7e9e1c5044cff1b2c8853b545a7315fe0f7
trizzi <patrickakhamiogu@gmail.comdquote>
2022-01-15 15:29:41 +0100

    Uploading backend files

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'welcome to the ShopRacksNg...' })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/home', require('./routes/home'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        'client',
+        'build',
+        'index.html'
+      )
+    )
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server started on ${PORT}`)
+);
